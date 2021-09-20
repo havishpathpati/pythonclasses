f15ede65f6acaca0704dec48e689915d40c6954b
cpathipati <ceeeyes@gmail.com>
2021-09-19 18:58:13 -0700

    addition calculator program

---
+++ cube.py
@@ -0,0 +1,17 @@
+#def cubes(a):
+	#return a ** 3
+
+#def sum(a,b):
+#	return int(a) + int(b)
+
+#val = input("enter the number:")
+#print(cubes(int(val)))
+
+#b=input("enter value for b:")
+#print(sum(a, b))
+def addition(num,num2):
+	return int(num)+int(num2)
+
+num=input("enter number 1:")
+num2=input("enter number 2:")
+print(addition(num,num2))
